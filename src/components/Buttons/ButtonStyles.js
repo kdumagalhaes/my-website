b6adae8313096d9772e206a808d3d18fd141706d
kdumagalhaes <ce.magalhaespereira@gmail.com>
2020-09-26 16:05:18 -0300

    add btn shadow and hover effect

--- src/components/Buttons/ButtonStyles.js
+++ src/components/Buttons/ButtonStyles.js
@@ -3,19 +3,21 @@ import styled from 'styled-components';
 export const ButtonTag = styled.button`
   width: 100%;
   max-width: ${({ maxWidth }) => maxWidth};
-  background-color: ${({backgroundColor}) => backgroundColor};
+  background-color: ${({ backgroundColor }) => backgroundColor};
   border-radius: 5px;
   height: 50px;
   border: none;
-  box-shadow: 0px 0px 55px rgba(10, 132, 255, 0.3);
-  color: ${({textColor}) => textColor};
+  box-shadow: 0px 4px 0px 0px rgba(10, 132, 255, 0.5);
+  color: ${({ textColor }) => textColor};
   font-size: 1.2rem;
   font-weight: bold;
-  transition: all 0.3s;
-  margin: ${({margin}) => margin};
+  margin: ${({ margin }) => margin};
+  transform: translateY();
+  transition: box-shadow 0.2s, transform 0.2s;
 
   &:hover {
-    box-shadow: 0px 0px 15px rgba(10, 132, 255, 0.5);
+    box-shadow: none;
+    transform: translateY(4px);
     background-color: var(--primary-blue);
     color: var(--lighter-blue);
   }
